f9bcb1a6d0d3aeac227c11420998e8e1e8fb238c
Zeehy123 <Zeehy123@users.noreply.github.com>
2025-01-24 00:13:34 +0100

    backend

--- backend/portfolio_tracker/settings.py
+++ backend/portfolio_tracker/settings.py
@@ -170,7 +170,7 @@ SIMPLE_JWT = {
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # React frontend
+    "https://simple-portfolio-tracker-nine.vercel.app",  # React frontend
 ]
 
 
